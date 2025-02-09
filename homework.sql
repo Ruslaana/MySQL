c55646dd36e1f57aedeae38eb528d397610b60cc
Ruslana Onyshchuk <lalelya7@gmail.com>
2025-02-09 12:13:52 +0100

    correct mistake

--- homework.sql
+++ homework.sql
@@ -1,19 +1,19 @@
-SELECT COUNT(*) FROM tasks;
+SELECT COUNT(*) FROM task;
 
-SELECT COUNT(*) FROM tasks WHERE due_date IS NULL;
+SELECT COUNT(*) FROM task WHERE due_date IS NULL;
 
-SELECT * FROM tasks WHERE status = 'done';
+SELECT * FROM task WHERE status = 'done';
 
-SELECT * FROM tasks WHERE status = 'not done';
+SELECT * FROM task WHERE status = 'not done';
 
-SELECT * FROM tasks ORDER BY created_at DESC;
+SELECT * FROM task ORDER BY created_at DESC;
 
-SELECT * FROM tasks ORDER BY created_at DESC LIMIT 1;
+SELECT * FROM task ORDER BY created_at DESC LIMIT 1;
 
-SELECT title, due_date FROM tasks WHERE title ILIKE '%database%' OR description ILIKE '%database%';
+SELECT title, due_date FROM task WHERE title ILIKE '%database%' OR description ILIKE '%database%';
 
-SELECT title, status FROM tasks;
+SELECT title, status FROM task;
 
-SELECT status, COUNT(*) FROM tasks GROUP BY status;
+SELECT status, COUNT(*) FROM task GROUP BY status;
 
-SELECT status, COUNT(*) AS count FROM tasks GROUP BY status ORDER BY count DESC;
+SELECT status, COUNT(*) AS count FROM task GROUP BY status ORDER BY count DESC;
